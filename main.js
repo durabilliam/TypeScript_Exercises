1b80d0d7146588e9fee90e47ef97ecba2035f2af
William Perry <durabilliam@gmail.com>
2021-09-16 12:03:29 -0230

    variable types

--- main.js
+++ main.js
@@ -1,2 +1,44 @@
-var message = 'Hello World!';
+"use strict";
+exports.__esModule = true;
+var message = 'Hello Buddy!';
 console.log(message);
+//Types
+var isBeginner = true;
+var total = 0;
+var name = 'Clark';
+var sentence = "My name is " + name + ". I am learning TypeScript!";
+console.log(sentence);
+//good for static type checking and intellisense (name.*) in VSC!!!
+//Sub-Types
+var n = null;
+var u = undefined;
+//null and undefined are subtypes of above and be assigned to all 3.
+var isNew = null;
+var myName = undefined;
+//Array-types
+var list1 = [1, 2, 3];
+var list2 = [1, 2, 3];
+var person1 = ['Clark', 99]; //Types and Values must match!!!
+//enum Type
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+var c = Color.Blue;
+console.log(c); // prints 2
+var Color2;
+(function (Color2) {
+    Color2[Color2["Red"] = 11] = "Red";
+    Color2[Color2["Green"] = 12] = "Green";
+    Color2[Color2["Blue"] = 13] = "Blue";
+})(Color2 || (Color2 = {}));
+;
+var d = Color2.Blue;
+console.log(d); //prints 13
+//Any Type
+var randomValue = 10;
+randomValue = true;
+randomValue = 'Clark';
--- main.ts
+++ main.ts
@@ -1,2 +1,38 @@
-let message = 'Hello World!';
+export{}
+let message = 'Hello Buddy!';
 console.log(message);
+//Types
+let isBeginner: boolean = true;
+let total: number = 0;
+let name: string = 'Clark';
+let sentence: string = `My name is ${name}. I am learning TypeScript!`;
+console.log(sentence);
+//good for static type checking and intellisense (name.*) in VSC!!!
+
+//Sub-Types
+let n: null = null;
+let u: undefined = undefined;
+//null and undefined are subtypes of above and be assigned to all 3.
+let isNew: boolean = null;
+let myName: string = undefined;
+
+//Array-types
+let list1: number[] = [1,2,3];
+let list2: Array<number> = [1,2,3];
+
+let person1: [string,number] = ['Clark', 99];   //Types and Values must match!!!
+
+//enum Type
+enum Color {Red, Green, Blue};
+let c: Color = Color.Blue;
+console.log(c);  // prints 2
+
+enum Color2 {Red = 11, Green, Blue};
+let d: Color2 = Color2.Blue;
+console.log(d); //prints 13
+
+//Any Type
+let randomValue: any = 10;
+randomValue = true;
+randomValue = 'Clark';
+
